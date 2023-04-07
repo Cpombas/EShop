2a3618254f92e9a92462adc303687bf40cc96787
Cpombas <128531098+Cpombas@users.noreply.github.com>
2023-04-07 03:31:57 +0100

    sksda

--- ProjetoFinal_LojaOnline/e-shopFE/src/app/checkout/checkout.component.ts
+++ ProjetoFinal_LojaOnline/e-shopFE/src/app/checkout/checkout.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/models';
+import { CartService } from '../services/cart/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  products: Product[] = [];
+
+  constructor(private cartService: CartService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.products = this.cartService.getCartProducts();
+  }
+
+  getTotalPrice(): number {
+    return this.products.reduce((total, product) => total + (product.price * product.quantity), 0);
+  }
+
+  confirmOrder(): void {
+    // Code to confirm the order goes here
+    this.router.navigate(['/payment']);
+  }
 
 }
